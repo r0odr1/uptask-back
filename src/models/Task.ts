11ceddff5ffd0254e838a5a877d0de7ebc38d190
Rodrigo Bonilla <rhbon.08@gmail.com>
2026-02-13 19:54:29 -0500

    Relacionando los modelos de tareas y proyectos

--- src/models/Task.ts
+++ src/models/Task.ts
@@ -1,8 +1,9 @@
-import mongoose, { Schema, Document } from "mongoose";
+import mongoose, { Schema, Document, Types } from "mongoose";
 
 export interface ITask extends Document {
   name: string
   description: string
+  project: Types.ObjectId
 }
 
 export const TaskSchema : Schema = new Schema({
@@ -16,7 +17,11 @@ export const TaskSchema : Schema = new Schema({
     trim: true,
     required: true
   },
-})
+  project: {
+    type: Types.ObjectId,
+    ref: 'Project',
+  },
+}, { timestamps: true })
 
 const Task = mongoose.model<ITask>('Task', TaskSchema)
 export default Task
