5f4488ae79cb3662e026b1d5652dc2d8ae8a0fed
Rodrigo Bonilla <rhbon.08@gmail.com>
2026-02-14 11:57:19 -0500

    Actualizar para obtener las tareas de un proyecto

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -31,7 +31,7 @@ export class ProjectController {
     const { id } = req.params
 
     try {
-      const project = await Project.findById(id)
+      const project = await Project.findById(id).populate('tasks')
 
       if(!project) {
         const error = new Error('Proyecto no encontrado')
