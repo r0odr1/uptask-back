d6168129ba38ca5d466f1dddde4e04045b81431c
Rodrigo Bonilla <rhbon.08@gmail.com>
2026-02-17 15:32:47 -0500

    Habilitando CORS para comunicar con el front

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,15 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cors from 'cors'
+import { corsConfig } from './config/cors'
 import { connectDB } from './config/db'
 import projectRoutes from './routes/projectRoutes'
 
 dotenv.config()
-
 connectDB()
 
 const app = express()
+app.use(cors(corsConfig))
 
 app.use(express.json())
 
