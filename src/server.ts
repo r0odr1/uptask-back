0bc85bf74357fca2fe352b0bfdbf71c28da3f2a6
Rodrigo Bonilla <rhbon.08@gmail.com>
2026-02-19 10:27:57 -0500

    Creando router para autenticacion y solucionando error de cors para pruebas con postman

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import morgan from 'morgan'
 import { corsConfig } from './config/cors'
 import { connectDB } from './config/db'
 import projectRoutes from './routes/projectRoutes'
+import authRoutes from './routes/authRoutes'
 
 dotenv.config()
 connectDB()
@@ -19,6 +20,7 @@ app.use(morgan('dev'))
 app.use(express.json())
 
 //Routes
+app.use('/api/auth', authRoutes)
 app.use('/api/projects', projectRoutes)
 
 export default app
