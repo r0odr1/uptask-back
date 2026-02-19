236fd22c94386c22e7c777997de7f82d1b7ae5fa
Rodrigo Bonilla <rhbon.08@gmail.com>
2026-02-19 14:25:59 -0500

    Hasheando el password de los usuarios

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,11 +1,16 @@
 import type { Request, Response } from 'express'
 import User from '../models/User'
+import { hashPassword } from '../utils/auth'
 
 export class AuthController {
   static createAccount = async (req: Request, res: Response) => {
 
     try {
+      const { password } = req.body
       const user = new User(req.body)
+
+      // Hash password
+      user.password = await hashPassword(password)
       await user.save()
 
       res.send('Cuenta creada, revisa tu correo para confirmarla')
