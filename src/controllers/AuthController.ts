80b87babc44b12769ed695d1e0944fd67aeebd08
Rodrigo Bonilla <rhbon.08@gmail.com>
2026-02-19 10:52:57 -0500

    Creando el controlador con validacion para la utenticacion

---
+++ src/controllers/AuthController.ts
@@ -0,0 +1,10 @@
+import type { Request, Response } from 'express'
+
+export class AuthController {
+  static createAccount = async (req: Request, res: Response) => {
+    res.send('Desde /api/auth')
+
+
+  }
+
+}
--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,9 +1,22 @@
 import { Router } from 'express'
+import { body } from 'express-validator'
+import { AuthController } from '../controllers/AuthController'
+import { handleInputErrors } from '../middleware/validation'
 
 const router = Router ()
 
-router.get('/', (req, res) => {
-  res.send('Desde /api/auth')
-})
+router.post('/create-account',
+  body('name').notEmpty().withMessage('El nombre no puede ir vacio'),
+  body('password').isLength({min:8}).withMessage('El password es muy corto, debe contar con minimo 8 caracteres'),
+  body('password_confirmation').custom((value, {req}) => {
+    if(value != req.body.password) {
+      throw new Error('Las constraseñas ingresadas no coinciden')
+    }
+    return true
+  }),
+  body('email').isEmail().withMessage('E-mail no válido'),
+  handleInputErrors,
+  AuthController.createAccount
+)
 
 export default router
