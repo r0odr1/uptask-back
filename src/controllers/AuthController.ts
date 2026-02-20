b6fc2b5398c842c71ca97faa63aafe111ac517bc
Rodrigo Bonilla <rhbon.08@gmail.com>
2026-02-20 12:14:25 -0500

    confirmar la cuenta si el token es valido

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -50,13 +50,18 @@ export class AuthController {
     try {
       const { token } = req.body
 
-      const tokenExist = await Token.findOne({token})
+      const tokenExists = await Token.findOne({token})
 
-      if(!tokenExist) {
+      if(!tokenExists) {
         const error = new Error('Token no valido')
         return res.status(401).json({error : error.message})
       }
 
+      const user = await User.findById(tokenExists.user)
+      user.confirmed = true
+
+      await Promise.allSettled([ user.save(), tokenExists.deleteOne() ])
+      res.send('Cuenta confirmada correctamente')
     } catch (error) {
       res.status(500).json.apply({error: 'Hubo un error'})
     }
