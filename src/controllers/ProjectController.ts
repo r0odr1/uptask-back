04f1d22d03bbd6bcaecd3a910d2747c20cf4bf70
Rodrigo Bonilla <rhbon.08@gmail.com>
2026-02-23 16:33:52 -0500

    Evitar que un usuario sin permisos actualice o elimine un proyecto

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -68,6 +68,11 @@ export class ProjectController {
         return res.status(404).json({ error: error.message })
       }
 
+      if(project.manager.toString() !== req.user._id.toString()) {
+        const error = new Error('Solo el Administrador del proyecto puede realizar cambios')
+        return res.status(404).json({ error: error.message })
+      }
+
       project.clientName = req.body.clientName
       project.projectName = req.body.projectName
       project.description = req.body.description
@@ -88,6 +93,11 @@ export class ProjectController {
 
       if(!project) {
         const error = new Error('Proyecto no encontrado')
+        return res.status(404).json({ error: error.message })
+      }
+
+      if(project.manager.toString() !== req.user._id.toString()) {
+        const error = new Error('Solo el Administrador del proyecto puede eliminar')
         return res.status(404).json({ error: error.message })
       }
 
