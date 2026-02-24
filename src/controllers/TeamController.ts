fe39ab31ad9b54fce24576756bb4825ab11d63ca
Rodrigo Bonilla <rhbon.08@gmail.com>
2026-02-23 19:11:18 -0500

    Creando endpoint para anadir miembros al proyecto

--- src/controllers/TeamController.ts
+++ src/controllers/TeamController.ts
@@ -11,10 +11,32 @@ export class TeamMemberControlle {
 
     if(!user) {
       const error = new Error('Usuario No Encontrado')
-
       return res.status(404).json({error: error.message})
     }
 
     res.json(user)
+  }
+
+  static addMemberById = async (req: Request, res: Response) => {
+
+    const { id } = req.body
+
+    // Find user
+    const user = await User.findById(id).select('id')
+
+    if(!user) {
+      const error = new Error('Usuario No Encontrado')
+      return res.status(404).json({error: error.message})
+    }
+
+    if(req.project.team.some(team => team.toString() === user.id.toString())) {
+      const error = new Error('El usuario ya existe en el proyecto')
+      return res.status(409).json({error: error.message})
+    }
+
+    req.project.team.push(user._id)
+    await req.project.save()
+
+    res.send('Usuario agregado correctamente')
   }
 }
--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -97,4 +97,10 @@ router.post('/:projectId/team/find',
   TeamMemberControlle.findMemberByEmail
 )
 
+router.post('/:projectId/team',
+  body('id').isMongoId().withMessage('ID no válido'),
+  handleInputErrors,
+  TeamMemberControlle.addMemberById
+)
+
 export default router
