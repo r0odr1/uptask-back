8a65a9401d8f87a784161915c74ca1750bd2ce26
Rodrigo Bonilla <rhbon.08@gmail.com>
2026-03-02 14:27:49 -0500

    permitiendo a los colaboradores ver el proyecto

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -24,7 +24,8 @@ export class ProjectController {
     try {
       const projects = await Project.find({
         $or: [
-          {manager: {$in: req.user._id}}
+          {manager: req.user._id},
+          {team: req.user._id}
         ]
       })
       res.json(projects)
@@ -45,7 +46,7 @@ export class ProjectController {
         return res.status(404).json({ error: error.message })
       }
 
-      if(project.manager.toString() !== req.user._id.toString()) {
+      if(project.manager.toString() !== req.user._id.toString() && !project.team.includes(req.user._id)) {
         const error = new Error('Proyecto no encontrado')
         return res.status(404).json({ error: error.message })
       }
